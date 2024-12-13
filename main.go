9ac349cc541c8484fa5b3107b4b1d41a619fa34c
Rajdeep Mandal <mrajdeep651@gmail.com>
2024-12-13 23:23:36 +0530

    Vendor form github

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("Initial RSS_Agg build")
+
+	godotenv.Load()
+
+	portStr := os.Getenv("PORT")
+	if portStr == "" {
+		log.Fatal("PORT not found")
+	}
+	fmt.Println("PORT: ", portStr)
 }
