231caff3421862c233cc8f37270d5b8f63b26702
Rajdeep Mandal <mrajdeep651@gmail.com>
2024-12-17 19:47:25 +0530

    log

--- main.go
+++ main.go
@@ -1,22 +1,32 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 	"os"
-	"github.com/joho/godotenv"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Initial RSS_Agg build")
-
 	godotenv.Load()
 
 	portStr := os.Getenv("PORT")
 	if portStr == "" {
 		log.Fatal("PORT not found")
 	}
-	
+
 	router := gin.New()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portStr,
+	}
+
+	log.Printf("Server starting on port %v", portStr)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
